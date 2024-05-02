89f5a1ded224f5753fae1b2b4a59827a0e698216
lKryml <gumimash@gmail.com>
2024-05-02 08:33:01 +0200

    initalized supabase connection + implemented user_exists function

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 import os
+import bcrypt
+from app.models import User
 from fastapi import FastAPI
-x = os.getenv("SUPABASE_URL")
-y = os.getenv("SUPABASE_KEY")
+from supabase import create_supabase_client
 
 app = FastAPI()
+
+supabase = create_supabase_client()
+
+def user_exists(key:str = "email", value:str =None ):
+    user= supabase.from_("users").select('*').eq(key,value ).execute()
+    return len(user.data)> 0
 
 @app.get("/")
 def main_route():
