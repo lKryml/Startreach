ff496d50be5fcc90de415782bdbc25b6e99157e0
Restyled.io <commits@restyled.io>
2024-05-02 06:49:17 +0000

    Restyled by reorder-python-imports

--- supabase/supabase.py
+++ supabase/supabase.py
@@ -1,6 +1,7 @@
 import os
 
-from supabase import Client, create_client
+from supabase import Client
+from supabase import create_client
 
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
