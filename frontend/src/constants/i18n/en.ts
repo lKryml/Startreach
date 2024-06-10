58a82c7d8087e9f620a01492879e29d66e71c4f1
Krym <gumimash@gmail.com>
2024-06-10 18:27:22 +0200

    i18n english -- sign up view optimized -- partners grayscale

--- frontend/src/constants/i18n/en.ts
+++ frontend/src/constants/i18n/en.ts
@@ -1,9 +1,179 @@
 export const en = {
-    AUTH: {
-        LOGIN: 'Login',
-        LOGOUT: 'Logout',
-        REGISTER: 'Sign Up',
-        FORGOT_PASSWORD: 'Did ypu forget your password?',
-        ENTER_DETAILS: "Enter your email below to login to your account"
-    }
+	MENU: {
+		HOME: "Home",
+		ABOUT: "Learn more",
+		DASHBOARD: "Dashboard",
+		PROJECTS: "Projects"
+	},
+	APP: {
+		NAME: "Startreach",
+		IMAGE_URL: ""
+	},
+	DASHBOARD: {
+		PAGE_TITLE: "Dashboard",
+		SHOW_IT: "Show Dashboard"
+	},
+	AUTH: {
+		LOGIN: "Login",
+		LOGOUT: "Logout",
+		REGISTER: "Sign Up",
+		CREATE_NEW_ACCOUNT: "Create a new Account  ",
+		HAV_ACCOUNT: "Already have an Account?   ",
+		FORGOT_PASSWORD: "Forgot your password?",
+		DONT_HAVE_ACCOUNT: "Join us, create your account now!",
+		ENTER_DETAILS: "Enter your email below to login to your account",
+		FAILED_LOGIN: "Login failed!",
+		FAILED_LOGIN_DESCRIPTION: "Please make sure you entered the correct login credentials",
+		EMAIL_ALREADY_EXISTS: "Email already exists",
+		FAILED_REGISTER: "Registration failed",
+		SUCCESS_REGISTER: "Registration successful",
+		FAILED_REGISTER_DESCRIPTION:
+			"Please make sure you entered the correct registration credentials",
+		WILL_BE_ROUTED_TO_HOME: "You will be redirected to the homepage"
+	},
+
+	USERS: {
+		FIRST_NAME: "First Name",
+		LAST_NAME: "Last Name",
+		EMAIL: "Email",
+		PASSWORD: "Password",
+		ADDRESS: "Address",
+		CONFIRM_PASSWORD: "Confirm Password",
+		USERNAME: "Username",
+		PHONE: "Phone Number",
+		USER_TYPE_1: "Entrepreneur",
+		USER_TYPE_2: "Company",
+		USER_TYPE_3: "Investor",
+		USER_TYPE_4: "Government",
+		USER_TYPE_5: "Organization",
+		USER_TYPE_6: "Educational",
+		USER_TYPE_7: "Union",
+
+		USER_TYPE_DESCRIPTION_1:
+			"You can view projects and get information to help you start your project",
+		USER_TYPE_DESCRIPTION_2: "",
+		USER_TYPE_DESCRIPTION_3: "View and browse startup projects for investment purposes",
+		USER_TYPE_DESCRIPTION_4:
+			"A government agency that seeks to provide information to businesses or investors",
+		USER_TYPE_DESCRIPTION_5:
+			"A government agency that seeks to provide information to businesses or",
+		USER_TYPE_DESCRIPTION_6:
+			"A government agency that seeks to provide information to businesses or",
+		USER_TYPE_DESCRIPTION_7:
+			"A government agency that seeks to provide information to businesses or"
+	},
+	PROFILE: {
+		NAME_1: "Name",
+		NAME_2: "Name",
+		NAME_3: "Name",
+		NAME_4: "Name",
+		NAME_5: "Name",
+		NAME_6: "Name",
+		NAME_7: "Name",
+		BIO_1: "Bio",
+		BIO_2: "Bio",
+		BIO_3: "Bio",
+		BIO_4: "Bio",
+		BIO_5: "Bio",
+		BIO_6: "Bio",
+		BIO_7: "Bio",
+		PHONE: "Phone Number",
+		FIRST_NAME: "First Name",
+		LAST_NAME: "Last Name",
+		EMAIL: "Email",
+		ADDRESS: "Address"
+	},
+	PROJECTS: {
+		PAGE_TITLE: "Projects",
+		SUB_TITLE:
+			"You can add and edit your projects to display them and explain the vision and everything related to the project",
+		ABOVE_LIST_SUB_TITLE:
+			"You can add and edit your projects to display them and explain the vision and everything related to the project",
+		SHOW: "Show Projects",
+		ADD_TITLE: "Project Data",
+		ADD_NEW: "Add New Project",
+		CREATE_NEW_PROJECT: "Save Project Data",
+		ENTER_DETAILS: "You can add a new project",
+		NAME: "Project Name",
+		LAUNCHE_DATE: "Launch Date",
+		UPLOAD_IMAGE_FOR_PROJECT_BG: "Upload an image for the project background",
+		IMAGE: "Project Image",
+		STATUS: "Status",
+		DESCRIPTION: "Description",
+		LUNCHED_DATE: "Launch Date",
+		CREATED_AT: "Created At"
+	},
+	WEBINARS: {
+		PAGE_TITLE: "Webinars",
+		DESCRIPTION: "Webinars created through this page",
+		SHOW: "Show Webinars",
+		ADD_NEW: "Add Webinar",
+		ENTER_DETAILS: "You can add a new webinar",
+
+		SUB_TITLE: "You can add and edit webinars created here",
+		ABOVE_LIST_SUB_TITLE:
+			"You can add and edit webinars to display them, explain their content, and related events",
+		ADD_TITLE: "Webinar Data",
+		CREATE_NEW_PROJECT: "Save Webinar Data",
+		NAME: "Webinar Name",
+		START_DATE: "Start Date",
+		END_DATE: "End Date",
+		UPLOAD_IMAGE_FOR_PROJECT_BG: "Upload an image for the webinar background",
+		IMAGE: "Webinar Image",
+		STATUS: "Status",
+		CREATED_AT: "Created At"
+	},
+
+	POSTS: {
+		PAGE_TITLE: "Blog",
+		POSTS: "Articles",
+		DESCRIPTION:
+			"Blog specific to the account where everything related to the account is published",
+		SHOW: "Show Articles",
+		ADD_NEW: "Add Article",
+		ENTER_DETAILS: "You can add a new article",
+
+		SUB_TITLE: "You can add and edit articles created here",
+		ABOVE_LIST_SUB_TITLE:
+			"You can add and edit articles to display them, explain their content, and related events",
+		ADD_TITLE: "Article Data",
+		CREATE_NEW_PROJECT: "Save Article Data",
+		TITLE: "Article Title",
+		START_DATE: "Start Date",
+		END_DATE: "End Date",
+		UPLOAD_IMAGE_FOR_PROJECT_BG: "Upload an image for the article background",
+		IMAGE: "Article Image",
+		STATUS: "Status",
+		CREATED_AT: "Created At"
+	},
+
+	GENERAL: {
+		ERROR: "Error",
+		INFO: "Information",
+		WARN: "Warning",
+		SUCCESS: "Success",
+		ACCEPT: "OK",
+		CANCEL: "Cancel",
+		ITEM_WILL_BE_DELETED: "Are you sure you want to delete this item permanently?",
+		EDIT_ITEM_GOES_WRONG: "Error while editing the item",
+		SUCCESSFULLY_EDIT_ITEM: "Item edited successfully",
+		YOU_R_OFFLINE: "Please check your internet connection",
+		FAIL_DELETE_ITEM: "Error deleting item",
+		FAIL_EDIT_ITEM: "Error editing item",
+		FAIL_ADD_ITEM: "Error adding item",
+		CONTACT_US: "Contact Us",
+		GET_STARTED: "Get Started Now",
+		LEARN_MORE: "Learn More",
+		LOADING_DATA: "Loading Data",
+		EXPORT: "Export",
+		FILTER: "Filter",
+		FILTER_BY: "Filter By",
+		SHOW_ACTIVE: "Active",
+		SHOW_INACTIVE: "Inactive",
+		SHOW_ALL: "All",
+		EDIT: "Edit",
+		DELETE: "Delete",
+		ACTOINS_TITLE: "Actions",
+		WAIT_SUBMITTING_DATA: "Saving Data"
+	}
 }
--- frontend/src/views/auth/AuthIndexView.vue
+++ frontend/src/views/auth/AuthIndexView.vue
@@ -3,23 +3,24 @@
 		<div class="hidden bg-muted lg:block">
 			<video
 				src="https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949"
-				class="h-full object-cover w-full bg-[url('https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949')] bg-[center_-283px]" autoplay
+				class="h-full object-cover w-full bg-[url('https://cdn.dribbble.com/uploads/48226/original/b8bd4e4273cceae2889d9d259b04f732.mp4?1689028949')] bg-[center_-283px]" autoplay loop="true" muted="true"
 			></video>
 		</div>
+		
 		<div class="flex items-center flex-col justify-start">
-			<div
-				class="w-full border-b-[1px] h-[60px] bg-accent text-accent-foreground border-b-border flex justify-end items-center px-10"
-			>
-				<router-link
+			
+				
+			
+			<router-view class="relative"/>
+			<router-link
 					to="/"
-					class="cursor-pointer py-2 px-6 rounded-full bg-primary text-primary-foreground hover:bg-secondary hover:text-secondary-foreground justify-center items-center flex flex-row-reverse"
-					><ArrowLeft />العودة للرئيسية</router-link
+					class="cursor-pointer  hover:text-secondary-foreground justify-center items-center flex flex-row-reverse self-start absolute left-1/4 top-52"
+					><PanelRightOpenIcon class="w-[24px] me-2 text-secondary hover:text-primary "></PanelRightOpenIcon></router-link
 				>
-			</div>
-			<router-view />
 		</div>
 	</div>
 </template>
 <script setup lang="ts">
 import { ArrowLeft } from "lucide-vue-next"
+import { PanelRightOpenIcon, SendIcon } from "lucide-vue-next"
 </script>
