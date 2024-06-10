ec948e44652f41f426ca53c7edd8f9535ca3ceed
Krym <gumimash@gmail.com>
2024-06-10 23:51:06 +0200

    projects dynamic

--- frontend/src/components/Projects.Component.vue
+++ frontend/src/components/Projects.Component.vue
@@ -31,13 +31,13 @@
 			
 			</div>
 			<div class="projects-section  justify-items-center bg-[url('/images/projects-hero22.png')] bg-no-repeat backy2">
-  <div class="project-card">
+  <div class="project-card" v-for="project in projects" :key="project.id">
     <div class="project-thumbnail">
-      <img src="/images/hd.jpg" alt="Project 1 thumbnail">
+      <img :src=project.img alt="Project 1 thumbnail">
     </div>
-    <h2 class="font-extrabold text-3xl">Chat GPT</h2>
-    <p class="subheading">Project Subheading 1</p>
-    <p>Project details go here. Lorem ipsum dolor sit amet...</p>
+    <h2 class="font-extrabold text-3xl">{{project.name}}</h2>
+    
+    <p>{{project.description}}</p>
     <button class="details-btn">View Details</button>
   </div>
   <div class="project-card">
@@ -70,8 +70,19 @@
 </div>
 		
 		</div>
-		
+		<div class="text-xs text-muted-foreground">
+							<PaginationComponent
+								:disabled="isLoadingList"
+								:items-perpage="pagination.per_page"
+								:total="pagination.total"
+								:current-page="pagination.page"
+								@onpagechanged="(page: number) => fetchData(page)"
+							></PaginationComponent>
+							<!-- Showing <strong>1-10</strong> of <strong>32</strong>
+							products -->
+						</div>
 	</section>
+  
 </template>
 <style scoped>
 .backy{
@@ -132,5 +143,116 @@
 }
 </style>
 <script setup lang="ts">
-let filter:string = 'all';
+let filter: string = 'all';
+import * as projectsService from "@/_services/projects.service"
+import { usePagination } from "@/_libs/usePagination"
+import { useRouter, useRoute } from "vue-router"
+import type { IProjects, IResponse } from "@/_interfaces"
+// import ItemCard from "@/components/ui/ItemCard.vue"
+import PanelTitle from "@/components/menu/PanelTitle.vue"
+import FilterComponent from "@/components/Filter.Component.vue"
+import PageIntroducerComponent from "@/components/ui/PageIntroducer.Component.vue"
+import PaginationComponent from "@/components/Pagination.Component.vue"
+import LoadingDataCardComponent from "@/components/ui/LoadingDataCard.Component.vue"
+import ConfirmComponent from "@/components/ui/Confirm.Component.vue"
+import ActionTableComponent from "@/components/ActionTable.Component.vue"
+import { useI18n } from "vue-i18n"
+import { onBeforeMount } from "vue"
+import { ref } from "vue"
+import { useToast } from "@/shared/shadcn-ui/ui"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from "@/shared/shadcn-ui/ui/card"
+import { Dialog, DialogContent } from "@/shared/shadcn-ui/ui"
+import ProjectsDetailsView from "../projects/ProjectsDetailsView.vue"
+
+onBeforeMount(async () => await fetchData())
+const route = useRoute()
+const router = useRouter()
+const { toast } = useToast()
+const { pagination } = usePagination(route)
+const { t } = useI18n()
+const isLoadingList = ref(false)
+const isShowDetailsDialog = ref(false)
+const cachedItem = ref<IProjects | any>(null)
+const isConfirmDeleteDialogOpen = ref(false)
+const projects = ref<IProjects[]>([])
+const fetchData = async (page?: number) => {
+	isLoadingList.value = true
+	if (page) {
+		await new Promise((resolve) => setTimeout(resolve, 500000000))
+	}
+	pagination.page = page || pagination.page
+	const response: IResponse<IProjects[]> | null = (await projectsService
+		.find(pagination)
+		.catch((err) => console.error(err))) as IResponse<IProjects[]>
+	isLoadingList.value = false
+	if (response?.data) {
+		projects.value = response.data || []
+		pagination.total = response.count || 0
+	}
+}
+
+const onCreateItem = (item?: IProjects) => {
+	if (item) {
+		isShowDetailsDialog.value = true
+		cachedItem.value = item as IProjects
+	} else router.push(`/dashboard/projects/details`)
+}
+const onDeleteItem = (item?: IProjects) => {
+	cachedItem.value = item as IProjects
+	isConfirmDeleteDialogOpen.value = true
+}
+const onDeleteItemComplete = async () => {
+	if (!cachedItem.value) return
+	isConfirmDeleteDialogOpen.value = false
+	isLoadingList.value = true
+	const response = await projectsService.destroy(cachedItem.value.id).catch(() => {})
+	console.log(response)
+	if (!response) {
+		isLoadingList.value = false
+		return toast({
+			variant: "destructive",
+			title: t("GENERAL.ERROR"),
+			description: t("GENERAL.FAIL_DELETE_ITEM"),
+			duration: 5000
+		})
+	}
+	await fetchData()
+	isLoadingList.value = false
+}
+
+const ItemsToShow = ref([
+	{ label: t("PROJECTS.NAME"), value: "name", type: "text" },
+	{ label: t("PROJECTS.STATUS"), value: "is_active", type: "badge" },
+	{
+		label: t("PROJECTS.LUNCHED_DATE"),
+		value: "created_at",
+		type: "date"
+	},
+	{
+		label: t("PROJECTS.CREATED_AT"),
+		value: "created_at",
+		type: "date"
+	}
+])
+
+const onTableRowAction = (action: string, payload?: IProjects) => {
+	switch (action) {
+		case "EDIT":
+			return onCreateItem(payload)
+		case "DELETE":
+			return onDeleteItem(payload)
+		default:
+			break
+	}
+}
+const onVisibileITemsChanged = (items: any) => {
+	return (ItemsToShow.value = items as any)
+}
 </script>
